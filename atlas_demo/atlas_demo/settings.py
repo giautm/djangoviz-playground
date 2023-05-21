b6c3bb57de883ab44f6a26cd1d9fe9a7920961f8
Giau. Tran Minh <hello@giautm.dev>
2023-05-21 17:15:41 +0000

    feat: config database driver for postgres
    Signed-off-by: Giau. Tran Minh <hello@giautm.dev>

--- atlas_demo/atlas_demo/settings.py
+++ atlas_demo/atlas_demo/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "atlas_demo.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+   "default": {
+       "ENGINE": "django.db.backends.postgresql_psycopg2",
+       "NAME": "postgres",
+       "USER": "postgres",
+       "PASSWORD": "pass",
+       "HOST": "db",
+       "PORT": "5432",
     }
 }
 
